753024039b46239bf1000a0a246a1298d1a13a1e
amar kumbar <137049443+Amarrk11@users.noreply.github.com>
2024-01-10 13:18:20 +0530

    done

--- src/com/jsp/git2/Test2.java
+++ src/com/jsp/git2/Test2.java
@@ -3,8 +3,8 @@ package com.jsp.git2;
 public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("asdf");
+		System.out.println("nanu amar");
 		System.out.println(";lkj");
-		System.out.println("change in shashi branch");
 	}
 
 }
